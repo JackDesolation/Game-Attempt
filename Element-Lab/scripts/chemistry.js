92e8321d17481f9d03c56428cf175dd7041055e2
JackDesolation <128559086+JackDesolation@users.noreply.github.com>
2026-01-27 21:45:24 -0600

    Delete Element-Lab/scripts/chemistry.js

--- Element-Lab/scripts/chemistry.js
+++
@@ -1,9 +0,0 @@
-const reactions = [
-    { inputs:["Hydrogen","Oxygen"], output:"Water (H₂O)", income:2 },
-    { inputs:["Carbon","Oxygen"], output:"Carbon Dioxide (CO₂)", income:3 },
-    { inputs:["Nitrogen","Hydrogen"], output:"Ammonia (NH₃)", income:4 },
-    { inputs:["Sodium","Chlorine"], output:"Sodium Chloride (NaCl)", income:5 },
-    { inputs:["Carbon","Hydrogen"], output:"Methane (CH₄)", income:3 },
-    { inputs:["Carbon","Nitrogen"], output:"Cyanogen (C₂N₂)", income:6 }
-];
-
