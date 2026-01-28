e6608fd4c5806295bbf8c28ec1fccddb2205632e
JackDesolation <128559086+JackDesolation@users.noreply.github.com>
2026-01-27 21:51:23 -0600

    Update data_reactions.js

--- Element-Lab/scripts/data_reactions.js
+++ Element-Lab/scripts/data_reactions.js
@@ -1,1 +1,18 @@
+const REACTIONS = [
+  { eq:"2H2+O2->2H2O", name:"Water", income:2 },
+  { eq:"C+O2->CO2", name:"Carbon Dioxide", income:3 },
+  { eq:"N2+3H2->2NH3", name:"Ammonia", income:4 },
+  { eq:"2Na+Cl2->2NaCl", name:"Sodium Chloride", income:5 },
+  { eq:"CH4+2O2->CO2+2H2O", name:"Methane Combustion", income:6 },
+  { eq:"CaCO3->CaO+CO2", name:"Calcium Oxide", income:6 },
+  { eq:"2K+2H2O->2KOH+H2", name:"Potassium Hydroxide", income:7 },
+  { eq:"SO3+H2O->H2SO4", name:"Sulfuric Acid", income:8 },
+  { eq:"N2+O2->2NO", name:"Nitric Oxide", income:7 },
+  { eq:"4Fe+3O2->2Fe2O3", name:"Iron Oxide", income:5 },
+  { eq:"C+H2O->CO+H2", name:"Water Gas", income:6 },
+  { eq:"CO+2H2->CH3OH", name:"Methanol", income:7 },
+  { eq:"2H2O->2H2+O2", name:"Electrolysis", income:5 },
+  { eq:"NH3+HCl->NH4Cl", name:"Ammonium Chloride", income:6 },
+  { eq:"NaOH+HCl->NaCl+H2O", name:"Neutralization", income:6 }
+];
 
