d00377893af63ddb1b9d879a19d796637651d845
JackDesolation <128559086+JackDesolation@users.noreply.github.com>
2026-01-27 21:40:49 -0600

    Update game.js

--- Element-Lab/scripts/game.js
+++ Element-Lab/scripts/game.js
@@ -1,1 +1,16 @@
+let funds = 0;
+let fundsPerSec = 0;
+
+let ownedElements = ["Hydrogen"];
+let compounds = [];
+let colliderBuilt = false;
+
+function recalcIncome() {
+    fundsPerSec = 0;
+    compounds.forEach(c => fundsPerSec += c.income);
+}
+
+setInterval(() => {
+    funds += fundsPerSec / 10;
+}, 100);
 
