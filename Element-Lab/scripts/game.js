2be209c961c70895d06ddae7ac4e9dd2cb433d4d
JackDesolation <128559086+JackDesolation@users.noreply.github.com>
2026-01-27 21:59:30 -0600

    Update game.js

--- Element-Lab/scripts/game.js
+++ Element-Lab/scripts/game.js
@@ -2,6 +2,7 @@ let funds = 0;
 let fps = 0;
 let owned = new Set(["H"]);
 let compounds = [];
+let compoundLibrary = [];
 let colliderBuilt = true;
 
 function recalc(){
@@ -11,3 +12,27 @@ function recalc(){
 setInterval(()=>{
   funds += fps/10;
 },100);
+
+// Auto-generate all possible valid compounds from owned elements
+function generateCompoundLibrary(){
+  compoundLibrary = [];
+  const ownedArr = Array.from(owned).map(s=>ELEMENTS.find(e=>e.symbol===s));
+
+  for(let i=0;i<ownedArr.length;i++){
+    for(let j=i;j<ownedArr.length;j++){
+      const a=ownedArr[i],b=ownedArr[j];
+      if(a.valence==0 || b.valence==0) continue;
+      // Simplified balancing: LCM of valences
+      const lcm=(x,y)=>x*y/ gcd(x,y);
+      const gcd=(x,y)=>!y?x:gcd(y,x%y);
+      const countA=lcm(a.valence,b.valence)/a.valence;
+      const countB=lcm(a.valence,b.valence)/b.valence;
+      const formula=countA>1?a.symbol+countA:a.symbol;
+      const formulaB=countB>1?b.symbol+countB:b.symbol;
+      const name=formula+formulaB;
+      const avgStability=(a.stability+b.stability)/2;
+      const income=Math.floor((avgStability+(a.Z+b.Z))/10);
+      compoundLibrary.push({name,ingredients:[a.symbol,b.symbol],income});
+    }
+  }
+}
